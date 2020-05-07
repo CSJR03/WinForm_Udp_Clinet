4a405ee78f3a052ae073cfd0cb884994e298c943
CSJR03 <v.sargsyan1996@gmail.com>
2020-05-07 19:26:24 +0400

    udp commit

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userNameTextBox = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.logoutButton = new System.Windows.Forms.Button();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.chatTextBox = new System.Windows.Forms.TextBox();
+            this.messageTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // userNameTextBox
+            // 
+            this.userNameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userNameTextBox.Location = new System.Drawing.Point(234, 419);
+            this.userNameTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.userNameTextBox.Multiline = true;
+            this.userNameTextBox.Name = "userNameTextBox";
+            this.userNameTextBox.Size = new System.Drawing.Size(389, 48);
+            this.userNameTextBox.TabIndex = 0;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginButton.Location = new System.Drawing.Point(631, 418);
+            this.loginButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(140, 49);
+            this.loginButton.TabIndex = 1;
+            this.loginButton.Text = "Login\r\n";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // logoutButton
+            // 
+            this.logoutButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logoutButton.Location = new System.Drawing.Point(955, 9);
+            this.logoutButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.logoutButton.Name = "logoutButton";
+            this.logoutButton.Size = new System.Drawing.Size(96, 62);
+            this.logoutButton.TabIndex = 2;
+            this.logoutButton.Text = "Out";
+            this.logoutButton.UseVisualStyleBackColor = true;
+            this.logoutButton.Click += new System.EventHandler(this.logoutButton_Click);
+            // 
+            // sendButton
+            // 
+            this.sendButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sendButton.Location = new System.Drawing.Point(657, 11);
+            this.sendButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(100, 49);
+            this.sendButton.TabIndex = 3;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // chatTextBox
+            // 
+            this.chatTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chatTextBox.Location = new System.Drawing.Point(16, 101);
+            this.chatTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.chatTextBox.Multiline = true;
+            this.chatTextBox.Name = "chatTextBox";
+            this.chatTextBox.Size = new System.Drawing.Size(1038, 309);
+            this.chatTextBox.TabIndex = 4;
+            // 
+            // messageTextBox
+            // 
+            this.messageTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.messageTextBox.Location = new System.Drawing.Point(234, 13);
+            this.messageTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.messageTextBox.Multiline = true;
+            this.messageTextBox.Name = "messageTextBox";
+            this.messageTextBox.Size = new System.Drawing.Size(415, 48);
+            this.messageTextBox.TabIndex = 5;
+            this.messageTextBox.TextChanged += new System.EventHandler(this.messageTextBox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.messageTextBox);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.chatTextBox);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.logoutButton);
+            this.Controls.Add(this.userNameTextBox);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox userNameTextBox;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Button logoutButton;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.TextBox chatTextBox;
+        private System.Windows.Forms.TextBox messageTextBox;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        bool alive = false;
+
+        UdpClient client;
+
+        const int LocalPort = 8001;
+        const int RemotePort = 8001;
+        const int TTL = 20;
+
+        const string Host = "235.5.5.1";
+        IPAddress groupAddress;
+
+        string userName;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            loginButton.Enabled = true;
+            logoutButton.Enabled = false;
+
+            sendButton.Enabled = false;
+            chatTextBox.ReadOnly = true;
+
+            groupAddress = IPAddress.Parse(Host);
+
+        }
+
+        void loginButton_Click(object sender , EventArgs e)
+        {
+            userName = userNameTextBox.Text;
+            userNameTextBox.ReadOnly = true;
+
+            try
+            {
+                client = new UdpClient(LocalPort);
+                //
+                client.JoinMulticastGroup(groupAddress, TTL);
+
+                //Task
+                Task receiveTask = new Task(ReceiveMessage);
+                receiveTask.Start();
+                //
+
+                string message = userName + " Mutq Gorcec chat";
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                client.Send(data, data.Length, Host, RemotePort);
+
+                loginButton.Enabled = false;
+                logoutButton.Enabled = true;
+                sendButton.Enabled = true;
+
+
+            }
+            catch(Exception mes)
+            {
+                MessageBox.Show(mes.Message);
+            }
+        }
+
+        void ReceiveMessage()
+        {
+            alive = true;
+            try
+            {
+                while (alive)
+                {
+                    IPEndPoint remoteIp = null;
+
+                    byte[] data = client.Receive(ref remoteIp);
+                    string message = Encoding.Unicode.GetString(data);
+
+                    this.Invoke(
+                        new MethodInvoker(
+                            () =>
+                            {
+                                string time = System.DateTime.Now.ToShortTimeString();
+                                chatTextBox.Text = time + " " + message + "\n" + chatTextBox.Text;
+                            }
+
+                            ));
+                        
+                }
+            }
+            catch(Exception mes)
+            {
+                MessageBox.Show(mes.Message);
+            }
+
+        }
+
+        void sendButton_Click(object sender,EventArgs e)
+        {
+            try
+            {
+                string message = $"{userName} : {messageTextBox.Text}";
+                byte[] data = Encoding.Unicode.GetBytes(message);
+
+                client.Send(data, data.Length, Host, RemotePort);
+                messageTextBox.Clear();
+            }
+            catch(Exception mes)
+            {
+                MessageBox.Show(mes.Message);
+            }
+        }
+
+        void logout_Click(object sender ,EventArgs e)
+        {
+            ExitChat();
+        }
+
+        void ExitChat()
+        {
+            string message = userName + " lqume chate";
+
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            client.Send(data, data.Length, Host, RemotePort);
+            client.DropMulticastGroup(groupAddress);
+
+            alive = false;
+
+            client.Close();
+
+            loginButton.Enabled = true;
+            logoutButton.Enabled = false;
+            sendButton.Enabled = false;
+
+        }
+
+        void Form1_FormClosing(object sender , FormClosingEventArgs e)
+        {
+            if (alive)
+            {
+                ExitChat();
+            }
+        }
+
+    }
+}
